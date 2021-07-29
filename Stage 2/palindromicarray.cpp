4dda96747007de3548d984daf8f61750fdd53a7e
Naveen Natarajan <58937732+naveenjack007@users.noreply.github.com>
2021-07-29 19:37:37 +0530

    Create palindromicarray.cpp

---
+++ Stage 2/palindromicarray.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> v(n);
+	    for(int i=0;i<n;i++){
+	        cin>>v[i];
+	    }
+	    int start = 0;
+	    int end = n-1;
+	    int cnt = 0;
+	    while(start < end){
+	        if(v[start] == v[end]){
+                start++;
+                end--;
+	        }
+	        if(v[start]>v[end]){
+	            v[end-1] = v[end-1]+v[end];
+	            end--;
+	            cnt++;
+	            continue;
+	        }
+	        else if(v[start]<v[end]){
+	            v[start+1] = v[start+1]+v[start];
+	            start++;
+	            cnt++;
+	            continue;
+	        }
+	    }
+	    cout<<cnt<<endl;
+	}
+	return 0;
+}
